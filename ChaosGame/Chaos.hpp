53d08966907404f325ddcc2c1aea41f0967e4661
Alex Wissing <awissing123@gmail.com>
2023-07-29 00:09:54 -0500

    swapped Chaos class into Sierpinski class and created a new chaos class that takes a set of transformations (functions that affect the movingVar). This generalization allows for this to also display the dragon curve and barnsely fern (though the barnsley fern looks odd in this so far). In Chaos::setPoint, there are modifying functions that attempt to position the movingVar coord into the range of the bitmap. Creating a temporary canvas that can be explored while the graphic is being created, and then saving whatever the current display is as a bitmap would avoid this annoying issue.

--- ChaosGame/Chaos.hpp
+++ ChaosGame/Chaos.hpp
@@ -1,104 +1,207 @@
 #pragma once
 
-#include <windows.h>
+#include <utility> // pair
+#include <stdexcept> // invalid_arugment
 #include <random>
-
+#include <string>
 #include "Bitmap.hpp"
 
-#include <iostream>
-
-constexpr unsigned int NUM_BOUNDS = 3;
-
-class Chaos {
-    Bitmap bmp;
-    const POINT bounds[NUM_BOUNDS];
-    const COLORREF colors[NUM_BOUNDS];
-    POINT movingVar;
-    std::mt19937 randomGen;
-    const long halfSize;
-
-public:
-    Chaos(unsigned int, std::mt19937&);
-    Chaos(Chaos&) = delete;
-    Chaos(Chaos&&) = default;
-    auto generate(unsigned int) -> void;
-    auto run(unsigned int) -> void;
-    auto save(std::string) const -> void;
-private:
-    auto setPoint(POINT&, POINT, int) -> void;
-    auto recursiveRun(unsigned int, unsigned int) -> void;
+struct Pair_F {
+	float x;
+	float y;
+	Pair_F(float x1, float y1) : x(x1), y(y1) {}
 };
 
-Chaos::Chaos(unsigned int bmpSize, std::mt19937& rGen)
-    : colors{ RGB(255, 0, 0), RGB(0, 255, 0), RGB(0, 0, 255) }
-    , halfSize((long) bmpSize >> 1)
-    , bounds{ // setup bounds for triangle (top middle, bottom left, bottom right)
-        { (long)bmpSize >> 1, 0 },                              // top middle
-        { 0, (long) bmpSize - 1 },                              // bottom left
-        { (long) bmpSize - 1, (long) bmpSize - 1 }              // bottom right
-    }
-    , randomGen(rGen)
+using Transformation = Pair_F(*)(const Pair_F&);
+
+using WeightedTransformation = std::pair<Transformation, unsigned int>;
+
+// used to generate Sierpinsky's triangle
+const WeightedTransformation SierpinskyTransformations[3] = {
+	std::make_pair(
+		[](const Pair_F& p) -> Pair_F {
+			return Pair_F(
+				(0 + p.x) / 2,
+				(0 + p.y) / 2
+			);
+		},
+		1
+	),
+	std::make_pair(
+		[](const Pair_F& p) -> Pair_F {
+			return Pair_F(
+				(1 + p.x) / 2,
+				(0 + p.y) / 2
+			);
+		},
+		1
+	),
+	std::make_pair(
+		[](const Pair_F& p) -> Pair_F {
+			return Pair_F(
+				(0.5 + p.x) / 2,
+				(1 + p.y) / 2
+			);
+		},
+		1
+	)
+};
+
+// used to generate Barnsley's fern
+const WeightedTransformation BarnsleyTransformations[4] = {
+	std::make_pair(
+		[](const Pair_F& p) -> Pair_F {
+			return Pair_F(
+				0,
+				0.16 * p.y
+			);
+		},
+		1
+	),
+	std::make_pair(
+		[](const Pair_F& p) -> Pair_F {
+			return Pair_F(
+				0.85 * p.x + (0.04 * p.y),
+				-0.04 * p.x + (0.85 * p.y) + 1.6
+			);
+		},
+		85
+	),
+	std::make_pair(
+		[](const Pair_F& p) -> Pair_F {
+			return Pair_F(
+				0.2 * p.x - (0.26 * p.y),
+				0.23 * p.x + (0.22 * p.y) + 1.6
+			);
+		},
+		7
+	),
+	std::make_pair(
+		[](const Pair_F& p) -> Pair_F {
+			return Pair_F(
+				-0.15 * p.x + 0.28 * p.y,
+				0.26 * p.x + (0.24 * p.y) + 0.44
+			);
+		},
+		7
+	)
+};
+
+// used to generate the Dragon curve
+const WeightedTransformation DragonCurveTransformations[2] = {
+	std::make_pair(
+		[](const Pair_F& p) -> Pair_F {
+			return Pair_F(
+				0.5 * p.x - 0.5 * p.y,
+				0.5 * p.x + 0.5 * p.y
+			);
+		},
+		1
+	),
+	std::make_pair(
+		[](const Pair_F& p) -> Pair_F {
+			return Pair_F(
+				-0.5 * p.x + 0.5 * p.y + 1,
+				-0.5 * p.x - 0.5 * p.y
+			);
+		},
+		1
+	)
+};
+class Chaos {
+	Bitmap bmp;
+	size_t bmpSize;
+	std::mt19937& gen;
+	WeightedTransformation* transformations;
+	size_t numTransformations;
+	Pair_F movingVar;
+	
+	public:
+		Chaos(unsigned int, std::mt19937&, WeightedTransformation*, size_t);
+		auto generate(unsigned int) -> void;
+		auto save(std::string) -> void;
+	private:
+		auto setPoint(Pair_F&, Transformation) -> void;
+		auto selectRandomlyWeighted(
+			const WeightedTransformation* w,
+			size_t n,
+			unsigned int tWeight = 0
+		) -> const Transformation&;
+};
+
+Chaos::Chaos(
+	unsigned int bmpSize,
+	std::mt19937& rGen,
+	WeightedTransformation* transforms,
+	size_t numTransforms
+) :
+	bmpSize(bmpSize)
+	, transformations(transforms)
+	, numTransformations(numTransforms)
+	, gen(rGen)
+	, movingVar(Pair_F(0.5, 0.5))
 {
-    this->movingVar = { this->halfSize, this->halfSize };
-    this->bmp.create(bmpSize, bmpSize);
-    this->bmp.clear(255);
+	this->bmp.create(bmpSize, bmpSize);
+	this->bmp.clear(255);
 }
 
-auto Chaos::save(std::string path) const -> void {
-    this->bmp.saveBitmap(path);
+auto Chaos::generate(unsigned int runs) -> void {
+	unsigned int cnt = 0, tWeight = 0, i;
+	for (auto i = 0; i < numTransformations; i++)
+		tWeight += transformations[i].second;
+	while (cnt++ < runs) {
+		setPoint(
+			this->movingVar,
+			this->selectRandomlyWeighted(
+				transformations,
+				numTransformations,
+				tWeight
+			)
+		);
+	}
 }
-auto Chaos::setPoint(POINT& o, POINT v, int i) -> void {
-    o.x = (o.x + v.x) >> 1; o.y = (o.y + v.y) >> 1; // avg o (moving var) with v (bounds point)
-    SetPixel(this->bmp.getDC(), o.x, o.y, this->colors[i]); // set color on bmp of given point. color driven by i
+auto Chaos::save(std::string s) -> void {
+	this->bmp.saveBitmap(s);
 }
-/*
-    So, this seems to randomly "eventually" generate all the pixel values
-    and paint them. However, there's not necessarily any reason this needs to be random.
-    With a Depth, the sequence of bounds selected can generate unique pixel values.
-    Ex. Depth: 2 is all sequences of length 2 of the bounds (combinations of bounds)
-    {
-        (0, 0)
-        (0, 1) // is this the same as (1, 0)? if so, permutations, else combinations
-        (0, 2)
-        (1, 0)
-        (1, 1)
-        (1, 2)
-        (2, 0)
-        (2, 1)
-        (2, 2)
-    }
-    sequence: [0, 0]
-    Suppose size = 100 and bounds[0] = 0,0 and movingVar = 50,50.
-    setPoint(movingVar, bounds[0], 0); // movingVar = 25,25
-    setPoint(movingVar, bounds[0], 0); // movingVar = 12,12
-    sequence: [1, 0, 1]
-    Suppose size = 100 and bounds[0] = 0,0 bounds[1] = 0,50 and movingVar = 50,50.
-    setPoint(movingVar, bounds[1], 0); // movingVar = 25, 50
-    setPoint(movingVar, bounds[0], 0); // movingVar = 12, 25
-    setPoint(movingVar, bounds[1], 0); // movingVar = 6, 37
-*/
-auto Chaos::generate(unsigned int runs) -> void { // randomly move to generate Sierpinski Triangle
-    unsigned int cnt = 0, i;
-    std::uniform_int_distribution<> dist(0, 5); // inclusive
-    while (cnt++ < runs) {
-        switch (dist(this->randomGen)) {
-            case 0: case 1: i = 0; break;
-            case 2: case 3: i = 1; break;
-            case 4: case 5: default: i = 2;
-        }
-        setPoint(this->movingVar, this->bounds[i], i);
-    }
+
+auto Chaos::setPoint(Pair_F& o, Transformation f) -> void {
+	o = f(o);
+	// sierpinski offset (none)
+	//float d1 = o.x;
+	//float d2 = o.y;
+	// barnsley offset
+	float d1 = 200 + (o.x * ((float)bmpSize) / 8);
+	float d2 = ((float)bmpSize) - (o.y * ((float)bmpSize) / 8);
+	// dragon offset
+	//float d1 = 200 + (o.x * ((float)bmpSize) / 3);
+	//float d2 = 300 + (o.y * ((float)bmpSize) / 3);
+	SetPixel(
+		this->bmp.getDC(),
+		d1,
+		d2,
+		RGB(255, 0, 0)
+	);
 }
-auto Chaos::run(unsigned int depth) -> void { // non randomly move to generate Sierpinski Triangle
-    this->recursiveRun(0, depth);
+auto Chaos::selectRandomlyWeighted(
+	const WeightedTransformation* w,
+	size_t n,
+	unsigned int tWeight
+) -> const Transformation& {
+	unsigned int totalWeight = tWeight;
+	if (tWeight == 0) // zero is an invalid total weight
+		for (auto i = 0; i < n; i++)
+			totalWeight += w[i].second;
+
+	std::uniform_int_distribution<> dist(0, totalWeight - 1);
+	unsigned int randVal = dist(gen);
+
+	unsigned int runningWeight = 0;
+	for (auto i = 0; i < n; i++) {
+		runningWeight += w[i].second;
+		if (randVal < runningWeight)
+			return w[i].first;
+	}
+	// if we get here, something went crazy bad, like overflow (or all weights are zero)
+	throw std::invalid_argument("Something went bad selecting a random weighted transformation");
 }
-auto Chaos::recursiveRun(unsigned int curr, unsigned int depth) -> void {
-    if (curr >= depth) return;
-    POINT temp;
-    for (unsigned int i = 0; i < NUM_BOUNDS; i++) {
-        temp = this->movingVar;
-        this->setPoint(this->movingVar, this->bounds[i], i);
-        this->recursiveRun(curr + 1, depth);
-        this->movingVar = temp;
-    }
-}
+
--- ChaosGame/ChaosGame.cpp
+++ ChaosGame/ChaosGame.cpp
@@ -4,14 +4,26 @@
 #include <iostream>
 
 #include "Chaos.hpp"
+#include "Sierpinski.hpp"
 
 int main(int argc, char* argv[]) {
     std::random_device rd;
 
     std::mt19937 gen(rd());
-    Chaos c = Chaos(500, gen);
+    /*
+        unsigned int bmpSize,
+	    std::mt19937& rGen,
+	    WeightedTransformation* transforms,
+	    size_t numTransforms
+    */
+    //Chaos c = Chaos(500, gen, (WeightedTransformation*) SierpinskyTransformations, 3);
+    Chaos c = Chaos(500, gen, (WeightedTransformation*) BarnsleyTransformations, 3);
+    //Chaos c = Chaos(1000, gen, (WeightedTransformation*) DragonCurveTransformations, 3);
+    c.generate(10000000);
+    c.save("C:/Users/Alex/Desktop/ChaosGameOutputBarnsley.bmp");
+    //Sierpinski c = Sierpinski(500, gen);
     //c.generate(1000000); // randomized
-    c.run(9); // deterministic
-    c.save("../chaosGameOutput.bmp");
+    //c.run(9); // deterministic
+    //c.save("../SierpinskiGameOutput.bmp");
     return 0;
 }
